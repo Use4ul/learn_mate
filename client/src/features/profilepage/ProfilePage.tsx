8a8c33c9ac16e4d792c97f338e45a618a4fcd119
Use4ul <i.am.use4ul@gmail.com>
2023-09-01 15:02:01 +0300

    navbar+link

---
+++ client/src/features/profilepage/ProfilePage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function ProfilePage(): JSX.Element {
+  return <div>ProfilePage</div>;
+}
+
+export default ProfilePage;
--- client/src/features/userpage/UserPage.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function UserPage(): JSX.Element {
-  return <div>UserPage</div>;
-}
-
-export default UserPage;
