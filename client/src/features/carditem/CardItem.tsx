3820ec76043bb75d47f39df020707ae4835aaf4f
Varvara Groznykh <varenkoluchka@gmail.com>
2023-09-01 15:30:44 +0300

    Components

--- client/src/features/carditem/CardItem.tsx
+++ client/src/features/carditem/CardItem.tsx
@@ -1,7 +1,24 @@
 import React from 'react';
 
 function CardItem(): JSX.Element {
-  return <div>CardItem</div>;
+  return (
+    <div className="card__container">
+      <h5>Название модуля</h5>
+      <div>
+        <div> Карточка
+          <button type="button"> назад</button>
+          <button type="button"> вперед</button>
+        </div>
+        <div>
+          <button type="button">Варианты обучения</button>
+          <button type="button">Варианты обучения</button>
+          <button type="button">Варианты обучения</button>
+        </div>
+      </div>
+      <div> Прогресс</div>
+
+    </div>
+  );
 }
 
 export default CardItem;
---
+++ client/src/features/profilepage/ProfilePage.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+
+function ProfilePage(): JSX.Element {
+  const [search, setSearch] = useState('');
+  return (
+    <div className="profil__container">
+      <div>
+        <input value={search} placeholder="введите название модуля" />
+
+        <div className="profil__module">
+          <div>
+            <button type="button">Создать новый модуль</button>
+          </div>
+          <div>
+            <div>Название</div>
+            <button type="button"> изменить</button>
+            <button type="button"> удалить</button>
+            <button type="button"> Назначить модуль группе</button>
+          </div>
+        </div>
+      </div>
+      <div>
+        <button type="button"> назначить модуль для обучения</button>
+        <div className="profil__grups">
+          <div>
+            <div>Название группы</div>
+            <button type="button"> изменить</button>
+            <button type="button"> удалить</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ProfilePage;
