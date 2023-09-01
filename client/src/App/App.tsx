be01f9c6badcffafdf64f54453c73a1d7d4614c6
Use4ul <i.am.use4ul@gmail.com>
2023-09-01 12:53:55 +0300

    all pages

--- client/src/App/App.tsx
+++ client/src/App/App.tsx
@@ -1,15 +1,20 @@
 /* eslint-disable no-undef */
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Navbar from '../features/navbar/Navbar';
 
 function App(): JSX.Element {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h2>Катя, Варя, Рустам</h2>
-        <h2>Welcome to LearnMate!!!</h2>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navbar />}>
+          {/* <Route path="/" element={<QuizePage />} />
+          <Route path="/auth/register" element={<RegForm />} />
+          <Route path="/auth/log" element={<LogForm />} /> */}
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/features/modulitem/ModulItem.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function ModulItem(): JSX.Element {
+  return <div>ModulItem</div>;
+}
+
+export default ModulItem;
