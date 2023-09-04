ae7468e9e08599aa07bdff1421f0bb2a86bcf5dc
locaust <kurbanov322@yandex.ru>
2023-09-04 12:12:49 +0300

    stage1

--- server/routes/api/user.routes.js
+++ server/routes/api/user.routes.js
@@ -13,4 +13,47 @@ router.get('/:userId/modules', async (req, res) => {
   }
 });
 
+router.get('/modules/:moduleId', async (req, res) => {
+  const { moduleId } = req.params;
+  try {
+    /*  const cardsInModule = await Card.findAll({ where: { module_id: +moduleId } });
+    res.json(cardsInModule); */
+    const cardsInModule = await Module.findAll({
+      where: { id: +moduleId },
+      include: {
+        model: Card,
+      },
+    });
+
+    res.json(cardsInModule);
+  } catch ({ message }) {
+    res.json({ message });
+  }
+});
+
+/* router.put('/modules/:moduleId', async (req, res) => {
+  try {
+    const { moduleId } = req.params;
+
+    const { title, description, img } = req.body;
+
+    const post = await Post.findOne({ where: { id: postId } });
+
+    if(post.user_id === req.session.user_id){
+
+    const [result] = await Post.update(
+      { title, description, img },
+      { where: { id: postId, user_id: req.session.user_id } },
+    );
+    if (result > 0) {
+      res.json({ message: 'success' });
+      return;
+    }
+  }
+    res.json({ message: 'false' });
+  } catch ({ message }) {
+    res.json(message);
+  }
+}); */
+
 module.exports = router;
