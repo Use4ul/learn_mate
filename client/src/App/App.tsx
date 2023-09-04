d649cea9c5edd17d2ae030d4892286ef08c456e7
locaust <kurbanov322@yandex.ru>
2023-09-04 16:02:07 +0300

    preloader

--- client/src/App/App.tsx
+++ client/src/App/App.tsx
@@ -6,12 +6,14 @@ import ProfilePage from '../features/profilepage/ProfilePage';
 import MainPage from '../features/mainpage/MainPage';
 
 import CardsPage from '../features/cardsPage/CardsPage';
-import { useAppDispatch } from '../redux/store';
-import { checkUser } from '../features/auth/reg/authSlice';
+import { RootState, useAppDispatch } from '../redux/store';
+import { checkUser, stopPending } from '../features/auth/reg/authSlice';
 import RegPage from '../features/auth/reg/RegPage';
 import LoginPage from '../features/auth/log/LoginPage';
 import useTheme from '../hooks/useTheme';
 import ModuleUpdateForm from '../features/profilepage/ModuleUpdatePage';
+import { useSelector } from 'react-redux';
+import preloader from './preloader.gif';
 
 function App(): JSX.Element {
   const dispatch = useAppDispatch();
@@ -19,18 +21,29 @@ function App(): JSX.Element {
   useEffect(() => {
     dispatch(checkUser());
   }, []);
+
+  const { pending } = useSelector((store: RootState) => store.auth);
+
+  useEffect(() => {
+    setTimeout(() => dispatch(stopPending()), 1000);
+  }, [pending]);
+
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Navbar />}>
-          <Route path="/profile/:userId/modules" element={<ProfilePage />} />
-          <Route path="/" element={<MainPage />} />
-          <Route path="/auth/register" element={<RegPage />} />
-          <Route path="/auth/log" element={<LoginPage />} />
-          <Route path="/modules/:moduleId" element={<CardsPage />} />
-          <Route path="/profile/:userId/modules/:moduleId" element={<ModuleUpdateForm />} />
-        </Route>
-      </Routes>
+      {pending ? (
+        <img src={preloader} alt="loader" />
+      ) : (
+        <Routes>
+          <Route path="/" element={<Navbar />}>
+            <Route path="/profile/:userId/modules" element={<ProfilePage />} />
+            <Route path="/" element={<MainPage />} />
+            <Route path="/auth/register" element={<RegPage />} />
+            <Route path="/auth/log" element={<LoginPage />} />
+            <Route path="/modules/:moduleId" element={<CardsPage />} />
+            <Route path="/profile/:userId/modules/:moduleId" element={<ModuleUpdateForm />} />
+          </Route>
+        </Routes>
+      )}
     </BrowserRouter>
   );
 }
