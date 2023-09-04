6e307f472729e4fa588ae3563a339df6430d4673
Use4ul <i.am.use4ul@gmail.com>
2023-09-04 17:02:24 +0300

    get_groupId

--- server/routes/api/groups.routers.js
+++ server/routes/api/groups.routers.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-const { User, Group, Role } = require('../../db/models');
+const { User, Group, GroupItem, Role } = require('../../db/models');
 
 router.get('/', async (req, res) => {
   const currentUser = await User.findOne({
@@ -21,6 +21,29 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.get('/:groupId', async (req, res) => {
+  const { groupId } = req.params;
+  const currentUser = await User.findOne({
+    where: { id: req.session.user_id },
+    include: { model: Role },
+  });
+  const currentGroup = await Group.findAll({
+    where: { id: groupId },
+    include: { model: GroupItem, include: { model: User } },
+  });
+  try {
+    if (currentUser && currentUser.Role.title === 'Учитель') {
+      res.json(currentGroup[0].GroupItems.map((el) => el.User.name));
+      return;
+    } else {
+      res.json({ message: 'Ты не учитель, куда лезешь!' });
+      return;
+    }
+  } catch ({ message }) {
+    res.json({ message });
+  }
+});
+
 router.delete('/:groupId', async (req, res) => {
   const { groupId } = req.params;
   try {
@@ -29,7 +52,7 @@ router.delete('/:groupId', async (req, res) => {
         where: { id: req.session.user_id },
       });
       const currentGroup = await Group.findOne({ where: { id: groupId } });
-      if (currentUser.Role.title === 'Учитель' && currentGroup.teacher_id === req.session.user_id) {
+      if (currentGroup.teacher_id === req.session.user_id) {
         const result = await Group.destroy({ where: { id: groupId } });
         if (result > 0) {
           res.status(200).json(+groupId);
@@ -70,24 +93,23 @@ router.post('/', async (req, res) => {
   }
 });
 
-// router.put('/:groupId', async (req, res) => {
-//   const { groupId } = req.params;
-//   const { title } = req.body;
-//   try {
-//     const group = await Group.findOne({ where: { id: groupId } });
-//     if (oneModule.user_id === req.session.user_id) {
-//       oneModule.title = title;
-//       oneModule.category = categoryId;
-//       oneModule.save();
-//       res.json(oneModule);
-//       return;
-//     } else {
-//       res.json({ message: 'Не отработал' });
-//       return;
-//     }
-//   } catch ({ message }) {
-//     res.json({ message });
-//   }
-// });
+router.put('/:groupId', async (req, res) => {
+  const { groupId } = req.params;
+  const { title } = req.body;
+  try {
+    const group = await Group.findOne({ where: { id: groupId } });
+    if (group.teacher_id === req.session.user_id) {
+      group.title = title;
+      group.save();
+      res.json(group);
+      return;
+    } else {
+      res.json({ message: 'Не отработал' });
+      return;
+    }
+  } catch ({ message }) {
+    res.json({ message });
+  }
+});
 
 module.exports = router;
