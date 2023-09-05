67f3e9dbe520bbf2f0669cbb0e9d3e0df4736a5a
locaust <kurbanov322@yandex.ru>
2023-09-05 12:00:18 +0300

    fixed bugs

--- client/src/features/carditem/CardItem.tsx
+++ client/src/features/carditem/CardItem.tsx
@@ -1,16 +1,40 @@
 /* eslint-disable no-undef */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Card } from '../cardsPage/types/types';
 import './styles/style.scss';
 
 function CardItem({ card }: { card: Card }): JSX.Element {
+  const [input, setInput] = useState(false);
+  const [rightAnswer, setRightAnswer] = useState('');
+  const [def, setDef] = useState('#222');
+
+  const func = () => {
+    setInput((prev) => !prev);
+    setDef('#fff');
+  };
+
+  useEffect(() => {
+    setInput(false);
+    setDef('#222');
+    setRightAnswer(card.definition);
+  }, [card]);
+
   return (
     <div className="card__container">
       <label>
-        <input type="checkbox" />
+        <input
+          checked={input}
+          /*  onChange={() => {
+            setInput((prev) => !prev), ;
+          }} */
+          onChange={func}
+          type="checkbox"
+        />
         <div className="card">
           <div className="front">{card.term}</div>
-          <div className="back">{card.definition}</div>
+          <div className="back" style={{ color: def }}>
+            {rightAnswer}
+          </div>
         </div>
         <div> Прогресс</div>
       </label>
