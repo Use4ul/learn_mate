be77ef8e084575d636015b0a5335c42f41cbccee
Ekaterina Bakhmeteva <ekaterina.kukhorevich@gmail.com>
2023-09-08 12:37:42 +0300

    stat bugs: fixed

--- client/src/features/404/404.tsx
+++ client/src/features/404/404.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 function Error(): React.JSX.Element {
   return (
     <div>
-        <h1>Такой страницы не существует</h1>
+      <h1>Такой страницы не существует</h1>
     </div>
-  )
+  );
 }
 
-export default Error
+export default Error;
